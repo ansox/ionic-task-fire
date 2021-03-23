cd11e348cc397394e3f07ce5cae5ff699b86d951
Anso <anderson.floriano@gmail.com>
2021-03-23 13:55:03 -0300

    first commit

---
+++ src/app/core/core.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { RouteReuseStrategy } from '@angular/router';
+import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
+
+@NgModule({
+  declarations: [],
+  imports: [
+    IonicModule.forRoot()
+  ],
+  exports: [
+    BrowserModule, 
+    IonicModule,
+  ],
+  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
+
+})
+export class CoreModule { }
